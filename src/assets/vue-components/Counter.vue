733d208ca3b40404981b01b8509bbf1e2bcf320a
diegofleitas <diego@design-it.de>
2019-11-27 16:43:00 -0300

    vue-custom-element demo

---
+++ src/assets/vue-components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>The count is: {{ count }}</h2>
+
+        <p>
+            <button @click="count++">Increment</button>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Counter",
+        data() {
+            return { count: 0 };
+        },
+        mounted() {
+            alert('Hello from the OTHER Vue side!');
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
